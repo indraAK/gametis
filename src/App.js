84eb57112d8b2596e52763d8be29bf405777e534
Indra Adi Kusuma <masjack@spidernet>
2021-11-23 23:27:23 +0700

    mengimport NotFound page component dan menambahkan 'Not Found' Routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Popular from "./pages/Popular";
 import Favorites from "./pages/Favorites";
 import Details from "./pages/Details";
+import NotFound from "./pages/NotFound";
 import Layout from "./components/layout/Layout";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="popular" element={<Popular />} />
         <Route path="favorites" element={<Favorites />} />
         <Route path="games/:id" element={<Details />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
   );
